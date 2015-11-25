184a992f463b2f035db6b2c240dda039715d2987
Akshay Karle <akshay.a.karle@gmail.com>
2015-11-25 13:37:36 -0300

    see if selenium uses chromedriver provided by snap

--- test/conf.js
+++ test/conf.js
@@ -1,7 +1,7 @@
 // Tests for the calculator.
 exports.config = {
   framework: 'jasmine2',
-  
+  chromeDriver = '/usr/local/bin/chromedriver',
   seleniumAddress: 'http://localhost:4444/wd/hub',
 
   specs: [
@@ -10,5 +10,5 @@ exports.config = {
 };
 
 if (process.env.SNAP_CI) {
-  exports.config.chromeDriver = '/usr/local/bin/chromedriver';
+
 }
