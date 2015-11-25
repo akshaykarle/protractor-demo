d3812a5133b0fec23b5f911d9dff80b9c8600d18
Akshay Karle <akshay.a.karle@gmail.com>
2015-11-25 13:53:37 -0300

    run with custom chromedriver only when in snap-ci

--- test/conf.js
+++ test/conf.js
@@ -2,9 +2,11 @@
 exports.config = {
   framework: 'jasmine2',
   
-  chromeDriver: '/usr/local/bin/chromedriver',
-  
   specs: [
     'spec.js'
   ]
 };
+
+if (process.env.SNAP_CI) {
+  exports.config.chromeDriver = '/usr/local/bin/chromedriver';
+}
